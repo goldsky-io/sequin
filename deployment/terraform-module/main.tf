8419b7ea69cf3532ef25bb318980221fa39bb627
Paymahn Moghadasian <paymahn1@gmail.com>
2025-09-25 08:33:06 -0500

    disable health check on metrics path

--- deployment/terraform-module/main.tf
+++ deployment/terraform-module/main.tf
@@ -811,15 +811,7 @@ resource "aws_lb_target_group" "sequin-metrics" {
   deregistration_delay = 60
 
   health_check {
-    enabled             = "true"
-    healthy_threshold   = "2"
-    interval            = "30"
-    matcher             = "200"
-    path                = "/metrics"
-    port                = "traffic-port"
-    protocol            = "HTTP"
-    timeout             = "5"
-    unhealthy_threshold = "5"
+    enabled = "false"
   }
 
   target_type     = "ip"
