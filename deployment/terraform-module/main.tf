fd9cd3ef6e748cd4b8e7d405cdfe263a0fe49328
Paymahn Moghadasian <paymahn1@gmail.com>
2025-09-25 08:40:21 -0500

    metrics health check hits regular health port

--- deployment/terraform-module/main.tf
+++ deployment/terraform-module/main.tf
@@ -811,7 +811,15 @@ resource "aws_lb_target_group" "sequin-metrics" {
   deregistration_delay = 60
 
   health_check {
-    enabled = "false"
+    enabled             = "true"
+    healthy_threshold   = "2"
+    interval            = "30"
+    matcher             = "200"
+    path                = "/health"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = "5"
+    unhealthy_threshold = "5"
   }
 
   target_type     = "ip"
